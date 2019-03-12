ab51bc7fe3a47d44951ee3e1da37a7e59a853f15
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-03-12 14:53:04 -0400

    Create grades.js

---
+++ grades.js
@@ -0,0 +1,76 @@
+
+var dataCSV = d3.csv("../data.csv");
+
+dataCSV.then(function(data)
+{
+  console.log("data",data);
+  drawChart(data, "svg");
+},
+function(err)
+{
+  console.log(err);
+});
+
+
+
+var drawChart = function(data,){
+  var height = 750;
+  var width = 1500;
+  var svg;
+  var barWidth = (width/data.length)
+    svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", function(d,i){
+              return i * barWidth + 10;
+            })
+            .attr("y", function(d){
+              return height - d.gini;
+            })
+            .attr("height", function(d){
+              return d.gini;
+            })
+            .attr("width", barWidth)
+            .attr("fill", "green");
+   
+  svg.selectAll("text.labels")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function(d){
+      return d.Country;
+    })
+    .attr("x", function(d, i){
+      return i * barWidth + 17;
+    })
+    .attr("y", function(d){
+      return height - d.gini + 15;
+    })
+    .attr("font-size", "12px")
+    .attr("fill", "white");
+  
+    svg.selectAll("text.num")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function(d){
+      return d.gini;
+    })
+    .attr("x", function(d, i){
+      return i * barWidth + 17;
+    })
+    .attr("y", function(d){
+      return height - d.gini + 30;
+    })
+    .attr("font-size", "12px")
+    .attr("fill", "white");
+
+
+ 
+
+}
