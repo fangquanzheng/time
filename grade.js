5a23ca37bddea24e7f28fb2efcc5210b17613df0
fangquanzheng <47224915+fangquanzheng@users.noreply.github.com>
2019-03-14 14:05:54 -0400

    Update grade.js

--- grade.js
+++ grade.js
@@ -1,4 +1,52 @@
+
 var grade = d3.json("gradedatatime.json");
+	var width = 500;
+	var chartW = 300;
+	var height = 250;
+	var barwidth = (chartW/7) - 5;
+	var legendW = 15;
+	var legendH = 15;
+	var legendpadding = 5;
+	var drawChart = function(data,day){
+
+	var svg = d3.select("svg");
+		svg.selectAll("rect")
+				.attr("width",width)
+				.attr("height",height);
+				
+				svg.selectAll("rect")
+					.data(data[day])
+					.enter()
+					.append("rect")
+					.attr("x",function(d,i){
+					  return i*(barwidth+5)
+					})
+					.attr("y",function(d){
+					  return height- d.grade*20
+					})
+					.attr("width",barwidth)
+					.attr("height",function(d){
+					  return d.grade*20
+					})
+					.attr("fill", "green");
+
+				// labels
+				svg.selectAll("text")
+					.data(data[day])
+					.enter()
+					.append("text")
+					.text(function(d){
+					  return d.name;
+					})
+					.attr("x",function(d,i){
+					  return i*(barwidth+5)+barwidth/2
+					})
+					.attr("y",function(d){
+					  return height- d.grade*20-5
+					})
+					.attr("text-anchor","middle")
+					.style("font-size",15)
+}
 grade.then(function(data)
 {
 	console.log("data",data);
@@ -8,56 +56,3 @@ function(err)
 {
 	console.log(err);
 });
-var width = 500;
-var chartW = 300
-var height = 250;
-var barpadding = 5;
-var barwidth = (chartW/7) - barpadding;
-var legendW = 15;
-var legendH = 15;
-var legendpadding = 5;
-var drawChart = function(data){
-var svg = d3.select("body")
-		.selectAll("svg")
-		.attr("width",width)
-		.attr("height",height);
-var svg = d3.select("body")
-				.selectAll("svg")
-				.attr("width",width)
-				.attr("height",height);
-				
-				svg.selectAll("rect")
-					.data(data)
-					.enter()
-					.append("rect")
-					.attr("x",function(d,i){
-					  return i*(barwidth+barpadding)
-					})
-					.attr("y",function(d){
-					  return height- d.num*20
-					})
-					.attr("width",barwidth)
-					.attr("height",function(d){
-					  return d.num*20
-					})
-					.attr("fill",function(d){
-					  return d.color
-					})
-
-				// labels
-				svg.selectAll("text")
-					.data(data)
-					.enter()
-					.append("text")
-					.text(function(d){
-					  return d.num;
-					})
-					.attr("x",function(d,i){
-					  return i*(barwidth+barpadding)+barwidth/2
-					})
-					.attr("y",function(d){
-					  return height- d.num*20-5
-					})
-					.attr("text-anchor","middle")
-					.style("font-size",15)
-})
